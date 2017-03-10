0f99efcdf3eda6b72f1b5b01d20758ff5704b68b
Khyber Sen <kkysen@gmail.com>
2017-03-09 19:44:32 -0500

    quiz 2 redux

---
+++ 06Quiz2Redux/Quiz2Redux.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 
+ * 
+ * @author Khyber Sen
+ */
+public class Quiz2Redux {
+    
+    public static String name() {
+        return "Sen,Khyber";
+    }
+    
+    public static ArrayList<String> combinations(final String s) {
+        final ArrayList<String> words = new ArrayList<>(1 << s.length());
+        help(s, words, s.length() - 1, "");
+        Collections.sort(words);
+        return words;
+    }
+    
+    private static void help(final String s, final List<String> words, final int i,
+            final String sub) {
+        if (i == -1) {
+            words.add(sub);
+            return;
+        }
+        help(s, words, i - 1, sub);
+        help(s, words, i - 1, sub + s.charAt(i));
+    }
+    
+    public static void main(final String[] args) {
+        final long start = System.currentTimeMillis();
+        System.out.println(combinations("abcdefghijklmnopqrst").size());
+        System.out.println((System.currentTimeMillis() - start) / 1e4);
+    }
+    
+}
